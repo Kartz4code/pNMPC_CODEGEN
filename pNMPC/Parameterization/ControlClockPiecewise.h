6e1bc8652f23a8c8318695be19a5b041975bbe3a
theMPCguy <karthikmr1991@gmail.com>
2020-07-04 16:22:48 +0200

    Update ControlClockPiecewise.h

--- pNMPC/Parameterization/ControlClockPiecewise.h
+++ pNMPC/Parameterization/ControlClockPiecewise.h
@@ -43,14 +43,13 @@ inline Real_C ControlClockPieceWise(Real_C current_clock, Real_C final_clock, Re
 			if (current_clock >= (Real_C)(CP[i] * final_clock) && current_clock <= (Real_C)(CP[i + 1] * final_clock))
 			{
 				u_clock = U_CP[i];
-				break;
+				return u_clock;
 			}
 			else if (current_clock >= (CP[N - 1] * final_clock))
 			{
 				u_clock = U_CP[N - 1];
-				break;
+				return u_clock;
 			}
 		}
 	}
-	return u_clock;
 }
